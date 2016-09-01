f471b66022cb2d3f94bfabb53f63fdee5527a8de
Sabrina Ooi <heysabs@gmail.com>
2016-09-01 16:09:49 +0800

    edit items

--- src/components/app.js
+++ src/components/app.js
@@ -28,15 +28,29 @@ export default class App extends React.Component {
         <CreateTodo createTask={this.createTask.bind(this)} />
         <TodosList
               todos={this.state.todos}
+              toggleTask={this.toggleTask.bind(this)}
+              saveTask={this.saveTask.bind(this)}
                />
       </div>
     );
   }
+  toggleTask(task) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task === task);
+    foundTodo.isCompleted = !foundTodo.isCompleted;
+    this.setState({todos: this.state.todos});
+  }
+
   createTask(task){
     this.state.todos.push({
       task,
       isCompleted: false
     });
     this.setState({ todos: this.state.todos });
+  }
+
+  saveTask(oldTask, newTask) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
+    foundTodo.task = newTask;
+    this.setState({ todos: this.state.todos })
   }
 }
